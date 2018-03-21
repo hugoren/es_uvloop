0ad60b841e70c4216cd95f227bab9d578b9c6dae
hugo <chenpengren@0easy.com>
2018-03-21 17:39:53 +0800

    rc

--- config.py
+++ config.py
@@ -3,9 +3,14 @@ import os
 env = os.getenv('ENV')
 
 if env == 'test':
-    HOST = '192.168.0.108'
-    PORT = 20000
+    HOST = '0.0.0.0'
+    PORT = 5454
     TOKEN = 'b0350c8c75ddcd99738df4c9346bec48dc9c4914'
+    REDIS_HOST = "192.168.6.23"
+    REDIS_PORT = 6379
+    REDIS_DB = 3
+    ES_HOST = "192.168.6.23"
+    ES_PORT = 9200
 
 
 elif env == 'prod':
@@ -17,9 +22,9 @@ else:
     HOST = '0.0.0.0'
     PORT = 5454
     TOKEN = 'b0350c8c75ddcd99738df4c9346bec48dc9c4914'
-    REDIS_HOST = "127.0.0.1"
+    REDIS_HOST = "192.168.6.23"
     REDIS_PORT = 6379
-    REDIS_DB = 0
+    REDIS_DB = 3
     ES_HOST = "192.168.6.23"
     ES_PORT = 9200
 
--- service.py
+++ service.py
@@ -1,14 +1,15 @@
-import asyncio
-import uvloop
 import time
-from datetime import datetime
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
-from concurrent.futures import ThreadPoolExecutor
+import json
+import uvloop
+import asyncio
+
 from blinker import signal
+from elasticsearch.helpers import bulk
+from elasticsearch import Elasticsearch
+from concurrent.futures import ThreadPoolExecutor
+
 from utils import log
 from utils import lpop_redis
-# from utils import write_to_es
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 loop = uvloop.new_event_loop()
@@ -29,38 +30,20 @@ def write_to_es(actions):
         log("error", str(e))
 
 
-async def pull_msg(bulk_num=50):
+async def pull_msg(bulk_num=5):
 
     try:
         actions = []
         while 1:
             msg = lpop_redis("log-msg")
-            _index = "log-{0}".format(time.strftime("%Y%m%d"))
-            _type = "log"
             if msg:
-                actions.append({
-                    "_index": _index,
-                    "_type": _type,
-
-                    "_source": {
-                        "timestamp": datetime.now(),
-                        "msg": str(msg, "utf-8"),
-                    }
-                })
-                if actions.__len__() >= bulk_num:
-                    # await write_to_es(_index, actions)
-                    start_time = time.time()
-                    es_signal.send(actions)
-                    actions.clear()
-                    print(time.time() - start_time)
-            else:
-                if actions:
-                    # await write_to_es(_index, actions)
+                actions.append(json.loads(msg))
+                if len(actions) >= bulk_num:
                     es_signal.send(actions)
                     actions.clear()
 
     except Exception as e:
-        print(e)
+        print("pull_msg", e)
         log('error', '写入es异常, 得有补偿机制, 异常原因:{0}'.format(str(e)))
 
 
